314ede85728e1f15d5f3181e5d87b361f7e53148
Maurice Smith <cactrot504@gmail.com>
2026-01-07 14:48:02 -0600

    Cosmetic change.  Updated site name from AngularBlog1 to AngularBlog

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-blog1'`, () => {
+  it(`should have as title 'angular-blog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-blog1');
+    expect(app.title).toEqual('angular-blog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-blog1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-blog app is running!');
   });
 });
